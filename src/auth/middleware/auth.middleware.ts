b623f205f1ca3b66b2fda72079472900ff71d713
Heorhii <jorhesancho@gmail.com>
2022-06-23 14:36:38 +0300

    edit exception filter

--- src/auth/middleware/auth.middleware.ts
+++ src/auth/middleware/auth.middleware.ts
@@ -9,13 +9,17 @@ export class AuthMiddleware implements NestMiddleware {
     }
 
     use(req: any, res: any, next: () => void) {
-        const token = req.headers?.authorization?.split(' ')[1];
-        if (!token) throw new HttpException('No token present', HttpStatus.FORBIDDEN)
-        const decodedToken: any = this.jwtService.verify(token, {publicKey: PUBLIC_KEY});
-        const userId = decodedToken?.id;
-        if (!decodedToken || typeof userId !== "number")
-            throw new HttpException('Invalid token', HttpStatus.FORBIDDEN)
-        res.locals.userId = userId;
-        next();
+        try {
+            const token = req.headers?.authorization?.split(' ')[1];
+            if (!token) throw new HttpException('No token present', HttpStatus.FORBIDDEN)
+            const decodedToken: any = this.jwtService.verify(token, {publicKey: PUBLIC_KEY});
+            const userId = decodedToken?.id;
+            if (!decodedToken || typeof userId !== "number")
+                throw new HttpException('Invalid token', HttpStatus.FORBIDDEN)
+            res.locals.userId = userId;
+            next();
+        } catch (e: any) {
+            throw new HttpException(e.message, HttpStatus.FORBIDDEN)
+        }
     }
 }
